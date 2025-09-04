fb0e4e87dbf3e040010d1310913143bc8d6a487f
mr-lubo <ruben.sethi@gmail.com>
2025-09-04 09:14:22 +0200

    update mobile page width

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import SchoenfeldClosedForm from './SchoenfeldClosedForm';
 function App() {
   return (
     <div className="App">
-      <div className="mx-auto p-6 bg-white rounded-lg shadow-md w-3/4">
+      <div className="mx-auto p-6 bg-white rounded-lg shadow-md w-full md:w-3/4">
         <h2 className="text-3xl mb-8">Estimate Survival Sample Size</h2>
           <SchoenfeldClosedForm />
       </div>
