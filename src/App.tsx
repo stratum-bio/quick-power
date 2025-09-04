d67c211d65c28a52e8a53f7b58fff5a391c4ada1
mr-lubo <ruben.sethi@gmail.com>
2025-09-04 12:45:11 +0200

    change css text to black

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="mx-auto p-6 bg-white rounded-lg shadow-md w-full md:w-3/4">
-        <h2 className="text-3xl mb-8">Estimate Survival Sample Size</h2>
+        <h2 className="text-3xl mb-8 text-black">Estimate Survival Sample Size</h2>
           <SchoenfeldClosedForm />
       </div>
     </div>
--- src/SchoenfeldClosedForm.tsx
+++ src/SchoenfeldClosedForm.tsx
@@ -63,7 +63,7 @@ const SchoenfeldClosedForm: React.FC = () => {
 
   return (
     <>
-      <div className="text-left mx-auto mb-4 px-4">
+      <div className="text-left mx-auto mb-4 px-4 text-black">
         <p>
           Schoenfeld's formula for estimating sample size from the 1983 paper "Sample-Size Formula for the Proportional-Hazards Regression Model".
           The first step is determining the total number of events required for a clinical trial that compares two survival distributions,
@@ -82,7 +82,7 @@ const SchoenfeldClosedForm: React.FC = () => {
           invalidMsg={invalidMsg}
         />
       </div>
-      <div className="text-left mx-auto mt-8 mb-8 px-4">
+      <div className="text-left mx-auto mt-8 mb-8 px-4 text-black">
         <p>
         Given the total number of events required from equation (1), you can compute the sample size by dividing this number by the proportion of expected
         events in the trial. The proportion events is calculated by first approximating the proportion of events on each treatment arm, and then taking a weighted
@@ -104,7 +104,7 @@ const SchoenfeldClosedForm: React.FC = () => {
           baseSurv={baseSurv} 
         />
       </div>
-      <div className="text-left mx-auto mt-8 mb-8 px-4">
+      <div className="text-left mx-auto mt-8 mb-8 px-4 text-black">
         <p>
         Here we are simply plotting the information provided. 
         <InlineMath math="\ S_B(t)" /> is directly entered in the second part of the form, then
