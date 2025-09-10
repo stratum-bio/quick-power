0c67c7c72c9cc50132ddefbe1d4f1f886b1599b9
mr-lubo <ruben.sethi@gmail.com>
2025-09-10 09:29:35 +0200

    format

--- src/constants.ts
+++ src/constants.ts
@@ -5,4 +5,11 @@ export const DISEASE_VAL_TO_NAME = {
   prostate_cancer: "Prostate",
 };
 
-export const PLOT_COLORS = ['#8884d8', '#82ca9d', '#ffc658', '#d0ed57', '#a4de6c', '#8dd1e1'];
+export const PLOT_COLORS = [
+  "#8884d8",
+  "#82ca9d",
+  "#ffc658",
+  "#d0ed57",
+  "#a4de6c",
+  "#8dd1e1",
+];
