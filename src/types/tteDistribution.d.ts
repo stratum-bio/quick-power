539367ef9f686643540b56ecb9e7c0a77e252f88
mr-lubo <ruben.sethi@gmail.com>
2025-09-10 19:55:48 +0200

    fix build

--- src/types/tteDistribution.d.ts
+++ src/types/tteDistribution.d.ts
@@ -2,7 +2,7 @@ import { type PValueDist } from "./simulation.d";
 
 export type TTEDistributionWorkerResult = PValueDist & {
   sampleSize: number;
-  baseInterval: [number, number];
-  treatInterval: [number, number];
-  pvalueInterval: [number, number];
+  baseInterval: number[];
+  treatInterval: number[];
+  pvalueInterval: number[];
 };
